d85486b8fdc6edfdd48031be1d2ec70101547b21
Arshad Kazmi <arshadkazmi42@gmail.com>
2019-01-26 17:07:48 +0530

    adds setup and contribution

---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="currency-symbols",
+    version="1.0.0",
+    author="Arshad Kazmi",
+    author_email="arshadkazmi42@gmail.com",
+    description="Get currency symbol by currency code",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/arshadkazmi42/currenct-symbols.py",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
