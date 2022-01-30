581ac58c21472839c8f7b10138e70d14296a1962
WardPearce <wardpearce@protonmail.com>
2022-01-31 12:08:48 +1300

    Static typing, flake8 liniting, docstrings & minor improvements

--- currency_symbols/__init__.py
+++ currency_symbols/__init__.py
@@ -1,1 +1,7 @@
+# -*- coding: utf-8 -*-
+
 from .currency_symbols import CurrencySymbols
+
+__all__ = [
+    "CurrencySymbols"
+]
---
+++ run_tests.py
@@ -0,0 +1,6 @@
+import unittest
+from currency_symbols.test import *  # noqa: F403 F401
+
+
+if __name__ == "__main__":
+    unittest.main()
