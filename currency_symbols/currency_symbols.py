76e52553dcaba28421ad7018f719e2c8154255a0
RamSurya01 <ramsurya.aks@gmail.com>
2024-06-16 21:48:44 +0530

    Function to convert Currency to Symbol

--- currency_symbols/currency_symbols.py
+++ currency_symbols/currency_symbols.py
@@ -21,3 +21,23 @@ class CurrencySymbols:
         """
 
         return CURRENCY_SYMBOLS_MAP.get(currency.upper(), None)
+
+    @staticmethod
+    def get_currency_code(currency_symbol: str) -> Optional[str]:
+        """Converts provided currency symbol into currency code.
+
+        Parameters
+        ----------
+        Currency symbol : str
+            ISO 4217 - Currency Codes
+
+        Returns
+        -------
+        Optional[str]
+            Currency, None if not exists.
+        """
+        for currency_code, symbol in CURRENCY_SYMBOLS_MAP.items():
+            if symbol == currency_symbol:
+                return currency_code
+            else:
+                return None
